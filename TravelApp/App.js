04b0d663880c45f06a6bac32c2c0498f4b7746c9
Sabrina <sabrina.taramasco@pyxis.com.uy>
2022-11-09 17:06:16 -0300

    final commit

--- TravelApp/App.js
+++ TravelApp/App.js
@@ -3,12 +3,22 @@ import Navbar from "./components/Navbar"
 import Card from "./components/Card"
 
 
-export default function App() {
-            // <Hero />
+
+export default function App () {
+    const cards = data.map(card=>{
+        return (
+            <Card 
+            key={card.id}
+            {...card}
+            />
+        )
+    })
     return (
         <div>
             <Navbar />
-            <Card />
+            <div className="cards-div">
+                {cards}
+            </div>
         </div>
     )
 }
